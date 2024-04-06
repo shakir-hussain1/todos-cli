55846f9d032fa038660307017c76e2c8dba2a171
marketplace <sadsain01@gmail.com>
2024-04-06 14:38:49 +0500

    push my todos to github

---
+++ index.js
@@ -0,0 +1,93 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.underline.bold.bgYellow("WELCOME TO TODOS APP"));
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoOperation = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: (chalk.red("Please select operation to do: ")),
+            choices: ["Add", "Delete", "Edit", "View", "Exit"]
+        }
+    ]);
+    // Add Task
+    if (todoOperation.operation === "Add") {
+        let addTask = await inquirer.prompt([
+            {
+                name: "addItem",
+                type: "input",
+                message: (chalk.green("please enter item to add in your todos? "))
+            },
+            {
+                name: "mainMenu",
+                type: "confirm",
+                message: (chalk.blue("please enter Y to main menu or N to exit ")),
+                default: "true"
+            }
+        ]);
+        todos.push(addTask.addItem);
+        console.log("Added Successfuly");
+        console.log(todos);
+        condition = addTask.mainMenu;
+    }
+    // Delete Task
+    else if (todoOperation.operation === "Delete") {
+        let deleteTask = await inquirer.prompt([
+            {
+                name: "deleteItem",
+                type: "input",
+                message: (chalk.magenta(`you have ${todos.length} items in your todos: press enter to delete first item`))
+            },
+            {
+                name: "mainMenu",
+                type: "confirm",
+                message: (chalk.cyan("please enter item to add in your todos? ")),
+                default: "true"
+            }
+        ]);
+        todos.splice(deleteTask.deleteItem, 1);
+        console.log("Deleted Successfuly");
+        console.log(todos);
+        condition = deleteTask.mainMenu;
+    }
+    //Edit Task
+    else if (todoOperation.operation === "Edit") {
+        let editTask = await inquirer.prompt([
+            {
+                name: "editItem",
+                type: "input",
+                message: (chalk.yellowBright(`you have ${todos.length} items in your todos, please enter item number to edit`))
+            },
+            {
+                name: "replaceItem",
+                type: "input",
+                message: (chalk.redBright("please enter item to update: "))
+            },
+            {
+                name: "mainMenu",
+                type: "confirm",
+                message: (chalk.grey("please enter item to edit in your todos? ")),
+                default: "true"
+            }
+        ]);
+        todos[editTask.editItem - 1] = editTask.replaceItem;
+        console.log("Edited Successfuly");
+        console.log(todos);
+        condition = editTask.mainMenu;
+    }
+    //view Task
+    else if (todoOperation.operation === "View") {
+        console.log(chalk.bgGreen("Here is your Todos"));
+        console.log(todos);
+    }
+    // Exit Task
+    else if (condition = false) {
+        console.log("Exit");
+    }
+    else {
+        console.log(chalk.bgMagenta("Its time to say Good Bye"));
+    }
+}
